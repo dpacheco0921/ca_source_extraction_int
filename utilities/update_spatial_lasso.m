5deb91fcca61d4e57f54eff7607fbb42f2a748ac
Diego A Pacheco Pinedo <dpacheco0921@gmail.com>
2017-02-22 14:26:48 -0500

    Revert "Update - fix bugs"
    This reverts commit 9891efa4f64c59c742253351a95745e1e99ec7eb.

--- utilities/update_spatial_lasso.m
+++ utilities/update_spatial_lasso.m
@@ -33,11 +33,6 @@ if   nargin<4 || isempty(IND); IND = determine_search_location(A,options.search_
 if nargin < 2 || isempty(A); 
     A = max(Y*C'/(C*C'),0);
 end
-if ~isfield(options,'spatial_parallel'); 
-    spatial_parallel = ~isempty(which('parpool')); 
-else
-    spatial_parallel = options.spatial_parallel;
-end % use parallel toolbox if present
 
 % if norm_C_flag
 %     nC = sqrt(sum(C.^2,2));
@@ -50,38 +45,20 @@ end % use parallel toolbox if present
 nr = K - options.nb;
 IND(:,nr+1:K) = true;
 T = size(C,2);
-step_size = 2e4;
 
 if memmaped
     %d = size(A,1);
-    sizY = Y.sizY;
-    if spatial_parallel
-        chunks = 1:step_size:d;
-        Yf = cell(numel(chunks),1); %zeros([d,numel(nr+1:K)]);
-        parfor t = 1:numel(chunks)
-            Yf{t} = double(Y.Yr(chunks(t):min(chunks(t)+step_size-1,d),:))*C(nr+1:end,:)';
-        end
-        Yf = cell2mat(Yf);
-    else
-        Yf = zeros([d,numel(nr+1:K)]);
-        for t = 1:step_size:d
-            Yf(t:min(t+step_size-1,d),:) = double(Y.Yr(t:min(t+step_size-1,d),:))*C(nr+1:end,:)';
-        end
+    step_size = 2e4;
+    YC = zeros([d,K]);    
+    for t = 1:step_size:d
+        YC(t:min(t+step_size-1,d),:) = double(Y.Yr(t:min(t+step_size-1,d),:))*C';
     end
-    YC = [];
 else
     YC = double(Y*C');
-    Yf = YC(:,nr+1:end); %Y*f';
 end
 
 %% initialization 
-if numel(A) > step_size
-    for i = 1:K
-        A(~IND(:,K),K) = 0;
-    end
-else
-    A(~IND) = 0; 
-end
+A(~IND) = 0; 
 U = YC; 
 V = C*C'; 
 cc = diag(V);   % squares of l2 norm for all components 
@@ -90,98 +67,22 @@ cc = diag(V);   % squares of l2 norm for all components
 miter = 0;
 while repeat && miter < maxIter
     A_ = A;
-    if spatial_parallel
-        Ai = cell(K,1);
-        tmp_ind = cell(K,1);
-        for k=1:K
-            tmp_ind{k,1} = IND(:,k);
-            Ai{k,1} = A(tmp_ind{k,1},:);
-            if mod(k,300) == 0
-                fprintf('%2.1f%% of pixels completed \n', k*100/K);
-            end
+    for k=1:K        
+        tmp_ind = IND(:, k);
+        if k <= nr
+            lam = sqrt(cc(k)); %max(sqrt(cc(tmp_ind)));
+        else
+            lam = 0;
         end
-        fprintf('\n')
-        parfor k=1:K
-            fprintf([num2str(k), '\n'])
-            % local computation of U
-            if memmaped
-                if k <= nr
-                    % Get subvolume
-                    sv_idx = getsubvolumeidx(tmp_ind{k,1}, sizY(1:end-1));
-                    tmp_ind_i = getsubvolume(tmp_ind{k,1}, sv_idx, sizY(1:end-1));
-                    tmp_ind_i = tmp_ind_i{1}; sv_idx = sv_idx{1};
-                    if size(sv_idx, 2) == 2
-                        Ul = Y.Y(sv_idx(1,1):sv_idx(2,1), sv_idx(1,2):sv_idx(2,2), :);
-                    else
-                        Ul = Y.Y(sv_idx(1,1):sv_idx(2,1), sv_idx(1,2):sv_idx(2,2), sv_idx(1,3):sv_idx(2,3), :);
-                    end
-                    Ul = reshape(Ul, numel(tmp_ind_i), T);
-                    Ul = double(Ul(tmp_ind_i>0,:)*C(k, :)');
-                else
-                    Ul = Yf(:, K - nr);
-                end
-            else
-                Ul = U(tmp_ind{k,1}, k);
-            end
-            
-            if k <= nr
-                lam = sqrt(cc(k)); %max(sqrt(cc(tmp_ind)));
-            else
-                lam = 0;
-            end
-            
-            LAM = norminv(q)*sn*lam;
-            
-            ak{k,1} = max(0, full(Ai{k,1}(:, k))+(Ul - LAM(tmp_ind{k,1}) - full(Ai{k,1}*V(:, k)))/cc(k));
-            
-            if mod(k,300) == 0
-                fprintf('%2.1f%% of pixels completed \n', k*100/K);
-            end           
-        end
-        fprintf('\n')
-        for k=1:K
-            A(tmp_ind{k,1}, k) = ak{k,1};
-            if mod(k,300) == 0
-                fprintf('%2.1f%% of pixels completed \n', k*100/K);
-            end
-        end
-        fprintf('\n')
-        clear tmp_ind ak
-    else
-        for k=1:K
-            fprintf('*')
-            tmp_ind = IND(:, k);
-            % local computation of U
-            if memmaped
-                % Get subvolume
-                sv_idx = getsubvolumeidx(tmp_ind, sizY(1:end-1));
-                tmp_ind_i = getsubvolume(tmp_ind, sv_idx, sizY(1:end-1));
-                tmp_ind_i = tmp_ind_i{1}; sv_idx = sv_idx{1};
-                if size(sv_idx, 2) == 2
-                    Ul = Y.Y(sv_idx(1,1):sv_idx(2,1), sv_idx(1,2):sv_idx(2,2), :);
-                else
-                    Ul = Y.Y(sv_idx(1,1):sv_idx(2,1), sv_idx(1,2):sv_idx(2,2), sv_idx(1,3):sv_idx(2,3), :);
-                end
-                Ul = reshape(Ul, numel(tmp_ind_i), T);
-                Ul = double(Ul(tmp_ind_i>0,:)*C(k, :)');
-                clear sv_idx tmp_ind_i
-            else
-                Ul = U(tmp_ind, k);
-            end
-            if k <= nr
-                lam = sqrt(cc(k)); %max(sqrt(cc(tmp_ind)));
-            else
-                lam = 0;
-            end
-            LAM = norminv(q)*sn*lam;
-            ak = max(0, full(A(tmp_ind, k))+(Ul - LAM(tmp_ind) - full(A(tmp_ind,:)*V(:, k)))/cc(k)); 
-            A(tmp_ind, k) = ak;
-        end
+        LAM = norminv(q)*sn*lam;
+        ak = max(0, full(A(tmp_ind, k))+(U(tmp_ind, k) - LAM(tmp_ind) - full(A(tmp_ind,:))*V(:, k))/cc(k)); 
+        A(tmp_ind, k) = ak; 
     end
     miter = miter + 1;
     repeat = (sqrt(sum((A(:)-A_(:)).^2)/sum(A_(:).^2)) > tol);    
 end
 
 f = C(nr+1:end,:);
+Yf = YC(:,nr+1:end); %Y*f';
 b = double(max((double(Yf) - A(:,1:nr)*double(C(1:nr,:)*f'))/(f*f'),0));
 A(:,nr+1:end) = b;
